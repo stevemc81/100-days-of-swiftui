ed98a01359a33ad1249d456646e4b6fc5211a19d
Steven McWee <stevenmcwee@gmail.com>
2023-01-20 15:12:57 +0000

    Day 24 - Create a custom modifier extending View to give a particular font style

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,10 +7,25 @@
 
 import SwiftUI
 
+struct BlueTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.blue)
+            .font(.largeTitle)
+    }
+}
+
+extension View {
+    func blueTitleStyle() -> some View {
+        modifier(BlueTitle())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .blueTitleStyle()
     }
 }
 
