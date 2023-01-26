f885cc7aab51af1210df29bb175c4b698a5abe2f
Steven McWee <stevenmcwee@gmail.com>
2023-01-26 09:01:42 +0000

    Day 27 - BetterRest initial commit

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Steven McWee on 25/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
