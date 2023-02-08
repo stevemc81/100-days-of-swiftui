ca473f12392c8c34721fdf906d49563a72e6a81e
Steven McWee <stevenmcwee@gmail.com>
2023-02-08 08:54:41 +0000

    iExpense - initial commit

---
+++ iExpense/iExpense/Expenses.swift
@@ -0,0 +1,31 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by Steven McWee on 07/02/2023.
+//
+
+import Foundation
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        
+        items = []
+    }
+}
