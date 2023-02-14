e96683cf878d8e1c4d10e8e520354687c467a716
Steven McWee <stevenmcwee@gmail.com>
2023-02-14 13:27:04 +0000

    Moonshot - initial commit

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Steven McWee on 13/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
