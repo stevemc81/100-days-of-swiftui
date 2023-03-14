973bc93ccc689be0c14e81914d0d3624ac14e427
Steven McWee <stevenmcwee@gmail.com>
2023-03-14 16:29:11 +0000

    Instafilter - initial commit

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Steven McWee on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
